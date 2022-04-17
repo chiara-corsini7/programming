73cee6625698459025daf7455db1992bcbe71262
Chiara Corsini <chiara.corsini7@studio.unibo.it>
2022-04-17 17:36:44 +0200

    added plotting of system

---
+++ project/roto-trasla/plot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 17 16:28:19 2022
+
+@author: chiaracorsini
+"""
+
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+import matplotlib.pyplot as plt
+
+
+    
+    
+
+def plot_molecule(a, b, c, el):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')     
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    
+    
+    colors = [float(hash(i) % 256) / 256 for i in el] 
+    
+    
+    ax.scatter3D(a, b, c, c=colors, cmap="jet")
+        
+#        for name in self.atoms_name:
+#            ax.scatter3D(atx,aty,atz, s=100, label=name)
+    ax.legend()
+    #plt.savefig('iimmaaggee.png')
