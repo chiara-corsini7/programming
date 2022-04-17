68818b90dd606a44449b5ac8fcb17cfba9dcb9cb
Chiara Corsini <chiara.corsini7@studio.unibo.it>
2022-04-17 17:34:02 +0200

    made it main part of code

--- project/roto-trasla/inputs.py
+++ project/roto-trasla/inputs.py
@@ -6,64 +6,65 @@ Created on Sat Mar 19 11:12:35 2022
 @author: chiaracorsini
 """
 
+import functions as fc
+import plot as plot
 import numpy as np
 import argparse
 
 
-def get_input():
-    parser = argparse.ArgumentParser(description='Translates and Rotates molecular coordinates.')
-    parser.add_argument('-f','--file', action='store', 
-                        help=".xyz file with coordinates", default='CO2.xyz', type=str)
-    parser.add_argument('-t','--translate', action='store', nargs=3, 
-                        help="x y and z coordinates of translation vector in $\AA$,"\
-                        " default 0. 0. 0.", default = [0.,0.,0.], type=float,
-                        metavar=('dx', 'dy', 'dz'))
-    parser.add_argument('-r','--rotate', action='store', nargs=3, 
-                        help="x y and z rotations in degrees,"\
-                        "default 0. 0. 0.", default =[0.,0.,0.], type=float,
-                        metavar=('rotx', 'roty', 'rotz'))
+
+parser = argparse.ArgumentParser(description='Translates and Rotates molecular coordinates.')
+parser.add_argument('-f','--file', action='store', 
+                    help=".xyz file with coordinates", default='CO2.xyz', type=str)
+parser.add_argument('-t','--translate', action='store', nargs=3, 
+                    help="x y and z coordinates of translation vector in $\AA$,"\
+                    " default 0. 0. 0.", default = [0.,0.,0.], type=float,
+                    metavar=('dx', 'dy', 'dz'))
+parser.add_argument('-r','--rotate', action='store', nargs=3, 
+                    help="x y and z rotations in degrees,"\
+                    "default 0. 0. 0.", default =[0.,0.,0.], type=float,
+                    metavar=('rotx', 'roty', 'rotz'))
         
-    args = parser.parse_args()
+args = parser.parse_args()
     #return(args)
 
 
 #def treat_input(args):
     # retrieviebìng 
-    modnt = np.array(args.translate)
-    modnr = np.array(args.rotate)
-    data = np.genfromtxt('CO2.xyz', skip_header=1, dtype='str')
+modnt = np.array(args.translate)
+modnr = np.array(args.rotate)
+data = np.genfromtxt(args.file, skip_header=1, dtype='str')
     
     
-    el = data[:,0]
-    a = data[:,1].astype(float)
-    b = data[:,2].astype(float)
-    c = data[:,3].astype(float)
-    
+el = data[:,0]
+a = data[:,1].astype(float)
+b = data[:,2].astype(float)
+c = data[:,3].astype(float)
+
 
     
-    return(modnt, modnr, el, a, b, c)
+print('Selected file: %s' % args.file)
+print('TRANSLATION %s in $\AA$' % modnt)
+print('ROTATION %s' % modnr)
+print('ELEMENTS %s' % el)
+print ('COORD X %s' % a)
+print ('COORD X %s' % b)
+print ('COORD X %s' % c)
     
+if (modnt==np.array([0.,0.,0.])).all() & (modnr==np.array([0.,0.,0.])).all():
+    print('Not translating nor rotating the molecule')
+elif (modnt==np.array([0.,0.,0.])).all():
+    print('Only rotating the molecule')
+    a_rot, b_rot, c_rot = fc.ruota(a, b, c, modnr)
+elif (modnr==np.array([0.,0.,0.])).all():
+    print('Only translating the molecule')
+    a_tr, b_tr, c_tr = fc.trasla(a, b, c, modnt)
+else:
+    print('Roto-translating the molecule')
+    a_rot, b_rot, c_rot = fc.ruota(a, b, c, modnr)
+    a_tr, b_tr, c_tr = fc.trasla(a_rot, b_rot, c_rot, modnt)
     
-    #parser.print_help()
-    print(args.translate)
-    #print(args.accumulate(args.integers))
-    
-    
-    
-def main():
-    #args = get_input()
-    modnt, modnr, el, a, b, c = get_input()
-    #modnt, modnr, el, a, b, c = treat_input(args)
-    print('TRANSLATION %s' % modnt)
-    print('ROTATION %s' % modnr)
-    print('ELEMENTS %s' % el)
-    print ('COORD X %s' % a)
-    print ('COORD X %s' % b)
-    print ('COORD X %s' % c)
-    
-
-if __name__ == '__main__':
-    main()
+plot.plot_molecule(a, b, c, el)
     
 
 #print("Coordinates loaded")
