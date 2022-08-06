4d7d3a56005e68c2eef8042a23fafdba9e98a97a
Chiara Corsini <chiara.corsini7@studio.unibo.it>
2022-08-06 11:15:27 +0200

    added test_trasla_replica

--- project/roto-trasla/test_comm.py
+++ project/roto-trasla/test_comm.py
@@ -17,6 +17,21 @@ def pytest_generate_tests(metafunc):
     if "fileName" in metafunc.fixturenames:
         filelist = glob.glob('test-files/*.xyz')
         metafunc.parametrize("fileName", filelist )
+    if "angle" in metafunc.fixturenames:
+        rd_angle = np.random.uniform(-360.,360., 3)
+        metafunc.parametrize('angle', [rd_angle])
+    if "modnt" in metafunc.fixturenames:
+        rd_modnt = np.random.uniform(-100000.,100000., 3)
+        metafunc.parametrize('modnt', [rd_modnt])
+    if "cell_vec" in metafunc.fixturenames:
+        rd_cell_vec = np.random.uniform(-10.,100., 3)
+        metafunc.parametrize('cell_vec', [rd_cell_vec])
+    if "cell_ang" in metafunc.fixturenames:
+        rd_cell_ang = np.random.uniform(0.,180., 3)
+        metafunc.parametrize('cell_ang', [rd_cell_ang])
+    if "modnre"  in metafunc.fixturenames:
+        rd_modnre = np.random.randint(1, 3, 3)
+        metafunc.parametrize('modnre', [rd_modnre])
         
 @pytest.fixture  
 def retreive_data(fileName):
@@ -29,33 +44,113 @@ def retreive_data(fileName):
     return(el, a, b, c)
 
 
-@pytest.mark.parametrize('modnt', [np.array([2., 7., 20.])])#, 
-                                   #np.array([-10., 70000., 0.])])
-@pytest.mark.parametrize('angle', [np.array([2., 7., 20.])])#, 
-                                   #np.array([-10., 70000., 0.])])
+# @pytest.mark.parametrize('modnt', [np.array([10., 100., -20.]), 
+#                                    np.array([0., 0., 0.])])
+# @pytest.mark.parametrize('angle', [np.array([-0.5, 90., -97.]), 
+#                                    np.array([0., 0., 0.])])
 @pytest.mark.parametrize('var', [True, False])
+
+
 def test_trasla_ruota(retreive_data, modnt, angle, var):
     
     if var == True:
-        el, a_iniz, b_iniz, c_iniz = retreive_data
+        el_iniz, a_iniz, b_iniz, c_iniz = retreive_data
         angle_rad = dt.angle_rad(angle)
     
         a_rot, b_rot, c_rot = fc.ruota(a_iniz, b_iniz, c_iniz, angle_rad, var)
         a_tr, b_tr, c_tr = fc.trasla(a_iniz, b_iniz, c_iniz, modnt)
     
-        a_rot_tr, b_rot_tr, c_rot_tr = fc.ruota(a_tr, b_tr, c_tr, angle_rad, var)
-        a_tr_rot, b_tr_rot, c_tr_rot = fc.trasla(a_rot, b_rot, c_rot, modnt)
+        a_tr_rot, b_tr_rot, c_tr_rot = fc.ruota(a_tr, b_tr, c_tr, angle_rad, var)
+        a_rot_tr, b_rot_tr, c_rot_tr = fc.trasla(a_rot, b_rot, c_rot, modnt)
     
         np.testing.assert_array_almost_equal(a_rot_tr, a_tr_rot)
         np.testing.assert_array_almost_equal(b_rot_tr, b_tr_rot)
         np.testing.assert_array_almost_equal(c_rot_tr, c_tr_rot)
     else:
         pytest.skip("When not free molecule rot and translation are not commutative")
+
+
+
+# @pytest.mark.parametrize('modnt', [np.array([10., 100., -20.]), 
+#                                    np.array([0., 0., 0.])])
+# @pytest.mark.parametrize('cell_vec', [np.array([1., 1., 1.])])
+#                                     #np.array([900., 56., 23.])])
+
+# @pytest.mark.parametrize('cell_ang', [np.array([90., 90., 90.]),
+#                                   np.array([90., 30., 60.])])
+
+# @pytest.mark.parametrize('modnre', [np.array([2, 2, 3])])#,
+#                                     #np.array([2, 4, 5])])       
+# @pytest.mark.skip
+def test_trasla_replica(retreive_data, modnt, cell_vec, cell_ang, modnre):
     
+    try:
+        el_iniz, a_iniz, b_iniz, c_iniz = retreive_data
+        angle_rad = dt.angle_rad(cell_ang)
     
+        el_rep, a_rep, b_rep, c_rep = fc.replica(el_iniz, a_iniz, b_iniz, c_iniz, modnre, cell_vec, angle_rad)
+        a_tr, b_tr, c_tr = fc.trasla(a_iniz, b_iniz, c_iniz, modnt)
     
+        a_rep_tr, b_rep_tr, c_rep_tr = fc.trasla(a_rep, b_rep, c_rep, modnt)
+        el_tr_rep, a_tr_rep, b_tr_rep, c_tr_rep = fc.replica(el_iniz, a_tr, b_tr, c_tr, modnre, cell_vec, angle_rad)
     
+        np.testing.assert_array_almost_equal(a_rep_tr, a_tr_rep)
+        np.testing.assert_array_almost_equal(b_rep_tr, b_tr_rep)
+        np.testing.assert_array_almost_equal(c_rep_tr, c_tr_rep)
+
+    except ValueError:
+        pytest.xfail('ValueError')
+        pass
+
+ 
+
+  
+# @pytest.mark.parametrize('angle', [np.array([-0.5, 90., -97.])])#, 
+#                                   # np.array([0., 0., 0.])])
+# @pytest.mark.parametrize('var', [True, False])
+
+# @pytest.mark.parametrize('cell_vec', [np.array([1., 1., 1.])])#,
+#                                     #np.array([900., 56., 23.])])
+
+# @pytest.mark.parametrize('cell_ang', [np.array([90., 90., 90.])])#,
+#                                   #np.array([90., 30., 60.])])
+
+# @pytest.mark.parametrize('modnre', [np.array([1, 2, 1])])#,
+#                                     #np.array([2, 4, 5])])       
+
+# def test_ruota_replica(retreive_data, angle, var, cell_vec, cell_ang, modnre):    
     
+#     try:
+#         if var == True:
+            
+#             el_iniz, a_iniz, b_iniz, c_iniz = retreive_data
+#             cell_angle_rad = dt.angle_rad(cell_ang)
+#             angle_rad = dt.angle_rad(angle)
+            
+#             #cell_vec_ruot = np.zeros(3)
+            
+#             el_rep, a_rep, b_rep, c_rep = fc.replica(el_iniz, a_iniz, b_iniz, c_iniz, modnre, cell_vec, cell_angle_rad)
+#             #cell_vec_ruot[0], cell_vec_ruot[1], cell_vec_ruot[2] = fc.ruota(cell_vec[0], cell_vec[1], cell_vec[2], angle_rad, var)
+#             a_rot, b_rot, c_rot = fc.ruota(a_iniz, b_iniz, c_iniz, angle_rad, var)
+        
+#             a_rep_rot, b_rep_rot, c_rep_rot = fc.ruota(a_rep, b_rep, c_rep, angle_rad, var)
+#             el_rot_rep, a_rot_rep, b_rot_rep, c_rot_rep = fc.replica(el_iniz, a_rot, b_rot, c_rot, modnre, cell_vec_ruot, cell_angle_rad)
+        
+#             np.testing.assert_array_almost_equal(a_rep_rot, a_rot_rep)
+#             np.testing.assert_array_almost_equal(b_rep_rot, b_rot_rep)
+#             np.testing.assert_array_almost_equal(c_rep_rot, c_rot_rep)
+#             #np.testing.assert_array_almost_equal(el_rep, el_rot_rep)
+            
+#             # print('EL_INIZ', el_iniz)
+#             # print('EL_REP', el_rep)
+#             # print('EL_ROT_REP', el_rot_rep)
+        
+#         else:
+#             pytest.skip("When not free molecule rot and translation are not commutative")
+    
+#     except ValueError:
+#         pytest.xfail('ValueError')
+#         pass
     
     
     
